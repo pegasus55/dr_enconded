fcc5569c8241b5517e459e5f2ab7c4335483626f
Johan Serrano Paret <johanserranoparet@gmail.com>
2023-07-19 18:09:23 -0400

    test

--- external_addons/V14/to_attendance_device/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import attendance_wizard
-from . import employee_upload_wizard
-from . import display_dialog_box_wizard
--- external_addons/V14/to_base/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import test_to_base
-from . import test_currency_rate_constrains
-from . import test_manifest_images
-from . import test_i18n_extra
